57428f7c1ca8a8c255adc6fab29290b5d2195b45
2200703658 <natukundaphionah51@gmail.com>
2023-07-25 22:40:38 +0300

    added gradient in register_screen

--- lib/pages/Register_screen.dart
+++ lib/pages/Register_screen.dart
@@ -1,9 +1,9 @@
+import 'dart:ui'; // Import the dart:ui library for Color class
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firstpro/text_fields/my_button.dart';
 import 'package:firstpro/text_fields/square_frame.dart';
 import 'package:firstpro/text_fields/my_textfield.dart';
-// import 'signup_screen.dart';
 
 class RegisterScreen extends StatefulWidget {
   final Function()? onTap;
@@ -14,12 +14,12 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
-  //text editing controllers
+  // Text editing controllers
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
   final confirmPasswordController = TextEditingController();
 
-  //sign user up
+  // Method to sign user up
   void signUserUp() async {
     // Show a progress circle
     showDialog(
@@ -108,132 +108,145 @@ class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[300],
-      body: SafeArea(
-        //makes UI avoid the notch area
-        child: Center(
-          //center the icon
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                //basically empty space
-                const SizedBox(height: 25),
+      backgroundColor:
+          Colors.transparent, // Set the background color to transparent
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors
+                  .blueGrey, // You can specify multiple colors for the gradient
+              Colors.white38,
+            ],
+          ),
+        ),
+        child: SafeArea(
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  //basically empty space
+                  const SizedBox(height: 25),
 
-                //logo
-                const Icon(
-                  Icons.lock,
-                  size: 60,
-                ),
-
-                const SizedBox(height: 25),
-                Text(
-                  'Create an account',
-                  style: TextStyle(
-                    color: Colors.grey[700],
-                    fontSize: 16,
+                  //logo
+                  const Icon(
+                    Icons.lock,
+                    size: 60,
                   ),
-                ),
 
-                const SizedBox(height: 25), //sized box for email
-                //email
-                MyTextField(
-                  controller: emailController,
-                  hintText: 'Email:',
-                  obscureText: false,
-                ),
+                  const SizedBox(height: 25),
+                  Text(
+                    'Create an account',
+                    style: TextStyle(
+                      color: Colors.grey[700],
+                      fontSize: 16,
+                    ),
+                  ),
 
-                //password
-                const SizedBox(height: 10), //sized box for password
-                MyTextField(
-                  controller: passwordController,
-                  hintText: 'Password',
-                  obscureText: true,
-                ),
+                  const SizedBox(height: 25), //sized box for email
+                  //email
+                  MyTextField(
+                    controller: emailController,
+                    hintText: 'Email:',
+                    obscureText: false,
+                  ),
 
-                const SizedBox(height: 10), //sized box for confirm password
-                //Confirm password
-                MyTextField(
-                  controller: confirmPasswordController,
-                  hintText: 'Confirm Password',
-                  obscureText: true,
-                ),
+                  //password
+                  const SizedBox(height: 10), //sized box for password
+                  MyTextField(
+                    controller: passwordController,
+                    hintText: 'Password',
+                    obscureText: true,
+                  ),
 
-                const SizedBox(height: 25), //sized box for signup button
-                MyButton(
-                  text: 'Sign Up',
-                  ontap: signUserUp, //call the method signUpUser
-                ),
+                  const SizedBox(height: 10), //sized box for confirm password
+                  //Confirm password
+                  MyTextField(
+                    controller: confirmPasswordController,
+                    hintText: 'Confirm Password',
+                    obscureText: true,
+                  ),
 
-                const SizedBox(height: 30), //sized box for "or continue with "
+                  const SizedBox(height: 25), //sized box for signup button
+                  MyButton(
+                    text: 'Sign Up',
+                    ontap: signUserUp, //call the method signUpUser
+                  ),
 
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Row(
+                  const SizedBox(
+                      height: 30), //sized box for "or continue with "
+
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Divider(
+                            thickness: 0.5,
+                            color: Colors.grey[400],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                          child: Text(
+                            'or continue with',
+                            style: TextStyle(color: Colors.grey[700]),
+                          ),
+                        ),
+                        Expanded(
+                          child: Divider(
+                            thickness: 0.6,
+                            color: Colors.grey[400],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  const SizedBox(height: 30), // space this too
+
+                  //apple and google signin buttons
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      //for google
+                      SquareFrame(imagepath: 'lib/images/google.png'),
+
+                      SizedBox(width: 25), // to space the buttons
+
+                      //for apple
+                      SquareFrame(imagepath: 'lib/images/apple.png'),
+                    ],
+                  ),
+
+                  const SizedBox(height: 30), // space this too
+                  //already a member? login
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Expanded(
-                        child: Divider(
-                          thickness: 0.5,
-                          color: Colors.grey[400],
+                      Text(
+                        'Already have an account?',
+                        style: TextStyle(
+                          color: Colors.grey[700],
                         ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                        child: Text(
-                          'or continue with',
-                          style: TextStyle(color: Colors.grey[700]),
-                        ),
-                      ),
-                      Expanded(
-                        child: Divider(
-                          thickness: 0.6,
-                          color: Colors.grey[400],
+                      const SizedBox(width: 4), // space this too
+
+                      GestureDetector(
+                        onTap: widget.onTap,
+                        child: const Text(
+                          'Login now',
+                          style: TextStyle(
+                              color: Colors.blue, fontWeight: FontWeight.bold),
                         ),
                       ),
                     ],
                   ),
-                ),
-
-                const SizedBox(height: 30), // space this too
-
-                //apple and google signin buttons
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    //for google
-                    SquareFrame(imagepath: 'lib/images/google.png'),
-
-                    SizedBox(width: 25), // to space the buttons
-
-                    //for apple
-                    SquareFrame(imagepath: 'lib/images/apple.png'),
-                  ],
-                ),
-
-                const SizedBox(height: 30), // space this too
-                //already a member? login
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Already have an account?',
-                      style: TextStyle(
-                        color: Colors.grey[700],
-                      ),
-                    ),
-                    const SizedBox(width: 4), // space this too
-
-                    GestureDetector(
-                      onTap: widget.onTap,
-                      child: const Text(
-                        'Login now',
-                        style: TextStyle(
-                            color: Colors.blue, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
