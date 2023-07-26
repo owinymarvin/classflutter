579579c9ec7db7cb4c046d0657f91202e5cf0ca4
owiny marvin <owinymarvin@gmail.com>
2023-07-26 20:22:30 +0300

    more additions. Drawer header button

--- lib/homepage after login/main.dart
+++ lib/homepage after login/main.dart
@@ -4,19 +4,54 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
+    int _selectedIndex = 2;
+    const TextStyle optionStyle =
+        TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+    const List<Widget> _widgetOptions = <Widget>[
+      Text(
+        'Index 0: Home',
+        style: optionStyle,
+      ),
+      Text(
+        'Index 1: Business',
+        style: optionStyle,
+      ),
+      Text(
+        'Index 2: School',
+        style: optionStyle,
+      ),
+    ];
+
+    void _onItemTapped(int index) {
+      setState(() {
+        _selectedIndex = index;
+      });
+    }
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
+          // leading: Icon(Icons.account_circle_rounded),
+          title: Text('Car Towing Service'),
+          centerTitle: true,
+          backgroundColor: Colors.redAccent,
+          scrolledUnderElevation: 10.0,
+          bottomOpacity: 0.1,
           actions: [
             PopupMenuButton(
-              icon: Icon(Icons.list),
-              iconSize: 50,
+              icon: Icon(Icons.account_circle_rounded),
+              iconSize: 30,
               color: Colors.white,
               enableFeedback: true,
               elevation: 10,
@@ -49,13 +84,53 @@ class MyApp extends StatelessWidget {
               },
             ),
           ],
-          title: Text('Car Towing Service'),
-          centerTitle: true,
-          backgroundColor: Colors.redAccent,
-          scrolledUnderElevation: 10.0,
         ),
-        body: Container(
-          color: Colors.grey[400],
+        body: Center(
+          child: _widgetOptions[_selectedIndex],
+        ),
+        drawer: Drawer(
+          child: ListView(
+            // Important: Remove any padding from the ListView.
+            padding: EdgeInsets.zero,
+            children: [
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.grey[400],
+                ),
+                child: Text('More Options'),
+              ),
+              ListTile(
+                title: Text('Home'),
+                selected: _selectedIndex == 0,
+                onTap: () {
+                  // Update the state of the app
+                  _onItemTapped(0);
+                  // Then close the drawer
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: const Text('Business'),
+                selected: _selectedIndex == 1,
+                onTap: () {
+                  // Update the state of the app
+                  _onItemTapped(1);
+                  // Then close the drawer
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: const Text('School'),
+                selected: _selectedIndex == 2,
+                onTap: () {
+                  // Update the state of the app
+                  _onItemTapped(2);
+                  // Then close the drawer
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
         ),
         bottomNavigationBar: BottomAppBar(
           color: Colors.red,
