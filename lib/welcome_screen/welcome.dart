4b965c785cf9a99f41e3b61e8cfd7855e5209e8b
natukunda phionah <natukundaphionah51@gmail.com>
2023-08-12 12:32:16 +0300

    nice look

--- lib/welcome_screen/welcome.dart
+++ lib/welcome_screen/welcome.dart
@@ -38,60 +38,59 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SizedBox(height: 100), // Adjust the spacing as needed
-          Text(
-            "Empowering Your Journeys",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 32,
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
-              fontFamily: 'Montserrat',
-            ),
-          ),
-          Text(
-            "with Reliable Towing Solutions",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.grey,
-              fontFamily: 'Montserrat',
-            ),
-          ),
-          SizedBox(height: 20),
-          SvgPicture.asset(
-            'assets/images/tow1.svg',
-            semanticsLabel: 'Car towing image',
-            color: Colors.greenAccent,
-            width: 280,
-          ),
-          SizedBox(height: 20),
-          Column(
-            children: [
-              Text(
-                "Quick Assistance",
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "Empowering Your Journeys",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+                fontFamily: 'Montserrat',
               ),
-              SizedBox(height: 10),
-              Text(
-                "Get help fast! Our app connects you with reliable towing professionals in minutes.",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.grey,
-                ),
+            ),
+            Text(
+              "with Reliable Towing Solutions",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.black,
+                fontFamily: 'Montserrat',
               ),
-            ],
-          ),
-        ],
+            ),
+            SizedBox(height: 20),
+            SvgPicture.asset(
+              'assets/images/tow1.svg',
+              semanticsLabel: 'Car towing image',
+              color: Colors.greenAccent,
+              width: 280,
+            ),
+            SizedBox(height: 20),
+            Text(
+              "Quick Assistance",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+                fontFamily: 'Montserrat',
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "Get help fast! Our app connects you with reliable towing professionals in minutes.",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.black,
+                fontFamily: 'Montserrat',
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
