a1186bda4f1b13be5af27a466cd8db40e9e48e9c
natukunda phionah <natukundaphionah51@gmail.com>
2023-08-23 18:59:26 +0300

    google map

--- towdrivers/lib/All_screens/new_trip_screen.dart
+++ towdrivers/lib/All_screens/new_trip_screen.dart
@@ -1,4 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class NewTripScreen extends StatefulWidget {
   const NewTripScreen({super.key});
@@ -8,14 +13,52 @@ class NewTripScreen extends StatefulWidget {
 }
 
 class _NewTripScreenState extends State<NewTripScreen> {
+  final Completer<GoogleMapController> _controllerGoogleMap = Completer();
+  GoogleMapController? newTripGoogleMapController;
+
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+  //variables
+  Set<Marker> setOffMarkers = Set<Marker>();
+  Set<Circle> setOffCircles = Set<Circle>();
+  Set<Polyline> setOffPolyline = Set<Polyline>();
+  //list
+  List<LatLng> polyLinePositionCoodinates = [];
+  PolylinePoints polylinePoints = PolylinePoints();
+  double mapPadding = 0;
+  BitmapDescriptor? iconAnimatedMarker;
+  var geolocator = Geolocator();
+  Position? onlineDriverCurrentPosition;
+  String rideRequestStatus = "accepted";
+  String durationFromOriginToDestination = "";
+  bool isRequestDirectionDetails = false;
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('gdhfjkgh'),
-        ),
-        body: Text('data'),
+    bool darktheme =
+        MediaQuery.of(context).platformBrightness == Brightness.dark;
+    return Scaffold(
+      body: Stack(
+        children: [
+          //Google map
+          GoogleMap(
+              padding: EdgeInsets.only(bottom: mapPadding),
+              mapType: MapType.normal,
+              myLocationEnabled: true,
+              initialCameraPosition: _kGooglePlex,
+              markers: setOffMarkers,
+              circles: setOffCircles,
+              polylines: setOffPolyline,
+              onMapCreated: (GoogleMapController controller) {
+                _controllerGoogleMap.complete(controller);
+                newTripGoogleMapController = controller;
+                setState(() {
+                  mapPadding = 350;
+                });
+              }),
+        ],
       ),
     );
   }
